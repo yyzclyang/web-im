50ba40611c1e87dc4b028090256625f573a87950
yyzcl <yyzclyang@gmail.com>
2019-12-20 00:33:26 +0800

    feat(index)：使用 redux 注入 store

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { StoreType } from "@/store";
 import "./app.scss";
 
-const App: React.FunctionComponent = () => {
-  return <div className="page">web-im</div>;
+const App: React.FunctionComponent = ({}) => {
+  const sign = useSelector((state: StoreType) => state.sign.signState);
+  return <div className="page">登录状态：{sign}</div>;
 };
 
 export default App;
