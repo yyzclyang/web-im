c26cb0d6f687b695a809b7b9c9372a9dd96d6ecb
yyzcl <yyzclyang@gmail.com>
2019-12-21 01:44:27 +0800

    feat(utils)：添加工具函数

---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export { default as classNames } from "./classNames";
+export { default as scopedClassMaker } from "./scopedClassMaker";
---
+++ src/utils/scopedClassMaker.ts
@@ -0,0 +1,9 @@
+const scopedClassMaker = (prefix: string) => {
+  return (suffix?: string) => {
+    return [prefix.trim(), suffix !== undefined ? suffix.trim() : suffix]
+      .filter(Boolean)
+      .join("-");
+  };
+};
+
+export default scopedClassMaker;
