b07648d0f0fdb9c035f685db622e34d86be8dd4a
yyzcl <yyzclyang@gmail.com>
2019-12-21 23:28:10 +0800

    feat(page)：配置页面路由

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,18 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { StoreType } from "@/store";
-import { login } from "@/store/action";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { classNames, scopedClassMaker } from "@/utils";
+import { SignIn, SignUp } from "./page/sign";
 import "./app.scss";
 
+const sc = scopedClassMaker("web-im");
+
 const App: React.FunctionComponent = ({}) => {
-  const sign = useSelector((state: StoreType) => state.sign.signState);
-  const dispatch = useDispatch();
-
-  const [count, setCount] = React.useState<number>(1);
-
   return (
-    <div className="page">
-      <span>登录状态：{sign}</span>
-      <button
-        onClick={() => {
-          dispatch(login(count));
-          setCount((count) => count + 1);
-        }}
-      >
-        click
-      </button>
+    <div className={classNames(sc("container"))}>
+      <Router>
+        <Route path="/signIn" component={SignIn} />
+        <Route path="/signUp" component={SignUp} />
+      </Router>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "@/store";
 import App from "./app";
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <Provider store={store}>
