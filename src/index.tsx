483c7279e5c93ec28207184a2a18e95b2437ed34
yyzcl <yyzclyang@gmail.com>
2019-12-24 12:21:56 +0800

    feat(index)：添加 Loading 效果

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import store from "@/store";
 import App from "./app";
 import "antd/dist/antd.css";
+import Loading from "@/components/loading";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
+    <Loading />
   </Provider>,
   document.getElementById("root")
 );
