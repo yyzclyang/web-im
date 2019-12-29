09c4aeac7bdb3b8bb005a767c2a087fd45ad810a
yyzcl <yyzclyang@gmail.com>
2019-12-29 14:44:57 +0800

    feat(chat)：如果没有 token，就跳转登录页

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,28 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import { classNames, scopedClassMaker } from "@/utils";
+import { Route, useLocation, useHistory } from "react-router-dom";
+import { classNames, scopedClassMaker, tokenUtil } from "@/utils";
 import { SignIn, SignUp } from "./page/sign";
+import Chat from "./page/chat";
 import "./app.scss";
 
 const sc = scopedClassMaker("web-im");
 
 const App: React.FunctionComponent = ({}) => {
+  const location = useLocation();
+  const history = useHistory();
+  React.useEffect(() => {
+    const isChatPage = /^\/chat\/?S*/.test(location.pathname);
+    if (isChatPage && !tokenUtil.getToken()) {
+      history.push("/signIn");
+    }
+  }, [location, tokenUtil]);
   return (
     <div className={classNames(sc("container"))}>
-      <Router>
-        <Route exact path="/" component={SignIn} />
-        <Route path="/signUp" component={SignUp} />
-        <Route path="/signIn" component={SignIn} />
-      </Router>
+      <Route exact path="/" component={SignIn} />
+      <Route path="/signUp" component={SignUp} />
+      <Route path="/signIn" component={SignIn} />
+      <Route exact path="/chat" component={Chat} />
+      <Route path="/chat/:chatType/:chatId" component={Chat} />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@/store";
 import App from "./app";
@@ -7,8 +8,10 @@ import Loading from "@/components/loading";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
-    <Loading />
+    <Router>
+      <App />
+      <Loading />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
